b3dc87c93027d05a82cc1da4512d4247ff383655
Eduardo <e.alves.rj@outlook.com>
2024-01-19 02:03:29 -0300

    adicionando products e users

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -33,3 +33,34 @@ VALUES
 ('002', 'DualSense', '400.00', 'Controle para PS5/PC', 'https://live.staticflickr.com/65535/49747503557_fa12f113db_h.jpg');
 
 SELECT * FROM products;
+
+--Exercicio 1
+SELECT * FROM users;
+
+SELECT * FROM products;
+
+SELECT * FROM products
+WHERE name LIKE('%PS5%')
+
+INSERT INTO users(id, name, email, password, creat_at)
+VALUES
+('004', 'Eduardo', 'edu@gmail.com', 'alves123', CURRENT_TIMESTAMP),
+('005', 'Gabriela', 'gb@gmail.com', 'azeredo123', CURRENT_TIMESTAMP)
+
+INSERT INTO products(id, name, price, description, image_url)
+VALUES
+('003', 'Game', '350.00', 'God of War Ragnarock', 'https://www.giantbomb.com/a/uploads/original/33/338034/3388962-1216435496-SZRc7.jpg')
+
+DELETE FROM users
+WHERE id = '003'
+
+DELETE FROM products
+WHERE id = '002'
+
+UPDATE products
+SET 
+    name = 'Game', 
+    price = '300', 
+    description = 'Mortal Kombat 1', 
+    image_url = 'https://www.giantbomb.com/a/uploads/original/33/338034/3388962-1216435496-SZRc7.jpg'
+WHERE id = '003';
